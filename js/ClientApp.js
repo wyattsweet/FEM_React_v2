feb29194c6e17fb32990faed2da54470b7e04d60
wyattsweet <wyattsweet@gmail.com>
2017-02-28 20:44:19 -0800

    v2-6 one behind

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { HashRouter, Match } from 'react-router'
+import Landing from './Landing'
+import Search from './Search'
 import '../public/normalize.css'
 import '../public/style.css'
 
 const App = React.createClass({
   render () {
     return (
-      <div className='app'>
-        <div className='landing'>
-          <h1>svideo</h1>
-          <input type='text' placeholder='Search' />
-          <a>or Browse All</a>
+      <HashRouter>
+        {/* The is how you do comments in JSX */}
+        <div className='app'>
+          <Match exactly pattern='/' component={Landing} />
+          <Match pattern='/search' component={Search} />
         </div>
-      </div>
+      </HashRouter>
     )
   }
 })
