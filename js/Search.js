b88c96e29f9cd701bc67d7120f18e3638c41ee4f
wyattsweet <wyattsweet@gmail.com>
2017-03-02 22:38:45 -0800

    12

--- js/Search.js
+++ js/Search.js
@@ -2,16 +2,42 @@ import React from 'react'
 import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
-
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+
+// Below works, but should not be used. Want to leave it in just as
+// a reminder that forceUpdate() exists. Integrating with other libraries
+// would be the only time to use forceUpdate().
+
+//    this.state.searchTerm = event.target.value
+//    this.forceUpdate()
+  },
+
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} show={show} /> 
-          )
-        })}
+        <header>
+          <h1>svideo</h1>
+          <input type='text' placeholder='search' onChange={this.handleSearchTermChange} value={this.state.searchTerm} />
+        </header>
+        <div>
+          {preload.shows
+
+            // Cool es6 one liners
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+            )
+            })}
+        </div>
       </div>
     )
   }
---
+++ js/Search.spec.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
+import Search from './search'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
+//import renderer from 'react-test-renderer'
+
+
+// BAREBONES WAY OF DOING SNAPSHOT TEST WITHOUT
+// ENZYME
+
+//test('Search snapshot test', () => {
+//  const component = renderer.create(<Search />)    
+//  const tree = component.toJSON()
+//  expect(tree).toMatchSnapshot()
+//})
+
+test('Search snapshot test', () => {
+  const component = shallow(<Search />)    
+  const tree = shallowToJson(component) 
+  expect(tree).toMatchSnapshot()
+})
+
+
+test('search should render a ShowCard for each show', () => {
+  const component = shallow(<Search />)
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+
+test('Search should render correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change', {target: {value: searchWord}})
+  const showCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
+
+  expect(component.find(ShowCard).length).toEqual(showCount)
+})
+
+// in enzyme you can use renderer but it's much slower. useful if you need to interact with DOM apis because shallow render doesn't have any dom apis available.
+
+
+// static rendering is even slower, uses cheerio. 
